ed367d4c50f03254eedb3d5a129aa6346353cf40
andrew <andrewmuhn@gmail.com>
2023-04-05 16:24:52 -0700

    added helper function for fs and GET response for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,9 @@
 const notes = require('express').Router();
+const { readFromFile } = require('../helpers/fsUtils');
 
 notes.route('/')
   .get((req, res) => {
-
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   })
   .post((req, res) => {
 
